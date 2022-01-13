56af0fd6518a792c2ed3e6d37fa1ae10ecc8714f
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-01-13 13:50:58 -0800

    Create booking test data file

---
+++ src/testing-data/booking-test-data.js
@@ -0,0 +1,39 @@
+const bookings = [
+  {
+    "id": "5fwrgu4i7k55hl6sz",
+    "userID": 9,
+    "date": "2022/04/22",
+    "roomNumber": 15,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6x8",
+    "userID": 1,
+    "date": "2022/01/11",
+    "roomNumber": 20,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t6",
+    "userID": 13,
+    "date": "2022/01/10",
+    "roomNumber": 12,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t7",
+    "userID": 20,
+    "date": "2022/02/16",
+    "roomNumber": 7,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t8",
+    "userID": 1,
+    "date": "2022/02/05",
+    "roomNumber": 12,
+    "roomServiceCharges": []
+  },
+];
+
+export default {bookings};
