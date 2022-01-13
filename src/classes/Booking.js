5935a274f766c62408bb71e5e0b2a6916082936f
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-01-13 14:08:14 -0800

    Create Booking class constructor

--- src/classes/Booking.js
+++ src/classes/Booking.js
@@ -1,6 +1,9 @@
 class Booking {
   constructor(booking) {
-
+    this.id = booking.id;
+    this.userID = booking.userID;
+    this.date = booking.date;
+    this.roomNumber = booking.roomNumber;
   }
 }
 
