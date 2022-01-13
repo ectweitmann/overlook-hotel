54b4fcbfd976040b24c5e8cec56111238659d738
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-01-13 10:21:17 -0800

    Create testing-data folder and customer-test-data file

---
+++ src/testing-data/customer-test-data.js
@@ -0,0 +1,16 @@
+const customers = [
+  {
+    "id": 1,
+    "name": "Leatha Ullrich"
+  },
+  {
+    "id": 2,
+    "name": "Rocio Schuster"
+  },
+  {
+    "id": 3,
+    "name": "Kelvin Schiller"
+  }
+];
+
+export default {customers};
