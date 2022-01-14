8895c2b60f3bf54e2a55c5c7498e959eac39dbbe
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-01-13 16:21:57 -0800

    Create Hotel class test file

---
+++ test/hotel-test.js
@@ -0,0 +1,30 @@
+import chai from 'chai';
+const expect = chai.expect;
+import Hotel from '../src/classes/Hotel';
+import Room from '../src/classes/Room';
+import Booking from '../src/classes/Booking';
+import customerTestData from '../src/testing-data/customer-test-data';
+import bookingTestData from '../src/testing-data/booking-test-data';
+import roomTestData from '../src/testing-data/room-test-data';
+
+describe('Hotel', () => {
+  let hotel;
+
+  beforeEach(() => {
+    hotel = new Hotel(
+      roomTestData.rooms.map(room => new Room(room)),
+      bookingTestData.bookings.map(booking => new Booking(booking)),
+      customerTestData.customers
+    );
+  });
+
+  describe('Properties', () => {
+
+
+  });
+
+  describe('Methods', () => {
+
+    
+  });
+});
