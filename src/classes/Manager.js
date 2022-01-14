d6b9dbcb1147c5ff271d334c3217b75650754545
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-01-14 11:45:11 -0800

    Create Manager class constructor and method(s)

--- src/classes/Manager.js
+++ src/classes/Manager.js
@@ -1,7 +1,19 @@
+import Customer from './Customer';
+
 class Manager {
   constructor(hotel) {
-
+    this.hotel = hotel;
+    this.selectedCustomer = null;
   }
+
+  selectCustomer(name) {
+    this.selectedCustomer = new Customer(this.hotel.guests.find(guest => guest.name === name));
+    this.selectedCustomer.getBookings(this.hotel);
+    this.selectedCustomer.determineTotalCost(this.hotel);
+  }
+
+  // ADD METHODS TO ADD/REMOVE A BOOKING ONCE NETWORK REQUEST FUNCTIONS ARE MADE
+
 }
 
 export default Manager;
