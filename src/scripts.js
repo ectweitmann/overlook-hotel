247749ffe03ebdbd6fd0ad403d061ebd0cc086ef
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-01-15 10:50:13 -0800

    Import bookings image

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,7 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './images/booking-image.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
