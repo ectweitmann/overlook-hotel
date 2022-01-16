4fb4c72fce86950397d46da2c21c46dd7c1bc682
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-01-16 11:15:39 -0800

    Create apiCalls object in which fetch calls liveas methods

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,47 +1,37 @@
 
-const getCustomers = () => {
-  return fetch('http://localhost:3001/api/v1/customers');
-}
+const apiCall = {
+  getCustomers() {
+    return fetch('http://localhost:3001/api/v1/customers');
+  },
+  getSingleCustomer(customerID) {
+    return fetch(`http://localhost:3001/api/v1/customers/${customerID}`);
+  },
+  getRooms() {
+    return fetch('http://localhost:3001/api/v1/rooms');
+  },
+  getBookings() {
+    return fetch('http://localhost:3001/api/v1/bookings');
+  },
+  addNewBooking(newBooking) {
+    return fetch('http://localhost:3001/api/v1/bookings', {
+      method: 'POST',
+      body: JSON.stringify(newBooking),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  },
+  deleteBooking(bookingID) {
+    return fetch(`http://localhost:3001/api/v1/bookings/${bookingID}`, {
+      method: 'DELETE',
+      body: JSON.stringify({
+        id: bookingID
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+};
 
-const getSingleCustomer = (customerID) => {
-  return fetch(`http://localhost:3001/api/v1/customers/${customerID}`);
-}
-
-const getRooms = () => {
-  return fetch('http://localhost:3001/api/v1/rooms');
-}
-
-const getBookings = () => {
-  return fetch('http://localhost:3001/api/v1/bookings');
-}
-
-const addNewBooking = (newBooking) => {
-  return fetch('http://localhost:3001/api/v1/bookings', {
-    method: 'POST',
-    body: JSON.stringify(newBooking),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-}
-
-const deleteBooking = (bookingID) => {
-  return fetch(`http://localhost:3001/api/v1/bookings/${bookingID}`, {
-    method: 'DELETE',
-    body: JSON.stringify({
-      id: bookingID
-    }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-}
-
-module.exports = {
-  getCustomers,
-  getSingleCustomer,
-  getRooms,
-  getBookings,
-  addNewBooking,
-  deleteBooking
-}
+module.exports = {apiCall};
