0dc9051bd0ce3521e0ee4a2efd1d58da615ab8a7
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-01-16 10:27:26 -0800

    Import api call functions and test their functionality

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,14 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import {
+  getCustomers,
+  getSingleCustomer,
+  getRooms,
+  getBookings,
+  addNewBooking,
+  deleteBooking
+} from './apiCalls';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
@@ -10,3 +18,35 @@ import './images/booking-image.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+getCustomers()
+  .then(data => console.log(data));
+
+getSingleCustomer(1)
+  .then(data => console.log(data));
+
+getRooms()
+  .then(data => console.log(data));
+
+getBookings()
+  .then(data => console.log(data));
+
+let newBooking = {
+  userID: 1,
+  date: '9999/09/09',
+  roomNumber: 999999999
+};
+
+// addNewBooking(newBooking)
+//   .then(booking => {
+//     console.log(booking)
+//     return getBookings();
+//   })
+//   .then(booking => console.log(booking));
+
+// deleteBooking("1642356832980")
+//   .then(data => {
+//     console.log(data);
+//     return getBookings();
+//   })
+//   .then(bookings => console.log(bookings));
