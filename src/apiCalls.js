3945c99cacddc9c6a81fdf362c2971c6b34382e3
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-01-16 09:55:55 -0800

    Add getSingleCustomer function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,12 @@ const getCustomers = () => {
   return fetch('http://localhost:3001/api/v1/customers');
 }
 
+const getSingleCustomer = (customerID) => {
+  return fetch(`http://localhost:3001/api/v1/customers/${customerID}`);
+}
+
 
 module.exports = {
-  getCustomers
+  getCustomers,
+  getSingleCustomer
 }
